7e91ebef4d4e34008f243b9098c4a378f7c27001
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2023-02-06 10:22:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const redis = require('redis');
 
-const REDISHOST = process.env.REDISHOST || '10.223.15.115';
+const REDISHOST = process.env.REDISHOST || '10.42.64.20';
 const REDISPORT = process.env.REDISPORT || 6379;
 
 const client = redis.createClient(REDISPORT, REDISHOST);
